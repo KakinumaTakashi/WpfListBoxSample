b11941ec52adbb34255003335c8c79c1ce5b1c9e
Kakinuma Takashi <kakinuma.takashi@homes.ecweb.jp>
2021-07-11 23:33:29 +0900

    初期登録

---
+++ WpfListBoxSample/MainWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp4;
+
+namespace WpfListBoxSample
+{
+    public class MainWindowViewModel : NotifyPropertyChanged
+    {
+        private MainWindow View { get; set; }
+
+
+        public class ListItem : NotifyPropertyChanged
+        {
+            public string Text1
+            {
+                get { return this._Text1; }
+                set
+                {
+                    this._Text1 = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+            private string _Text1;
+
+            public string Text2
+            {
+                get { return this._Text2; }
+                set
+                {
+                    this._Text2 = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+            private string _Text2;
+        }
+
+        public ObservableCollection<ListItem> ListItems { get; private set; }
+            = new ObservableCollection<ListItem>();
+
+
+        public MainWindowViewModel(MainWindow view)
+        {
+            this.View = view;
+
+            this.ListItems.Add(new ListItem() { Text1 = "AAAAAAAA", Text2 = "aaaaaaaaaaa" });
+            this.ListItems.Add(new ListItem() { Text1 = "BBBBBBBB", Text2 = "bbbbbbbbbbb" });
+            this.ListItems.Add(new ListItem() { Text1 = "CCCCCCCC", Text2 = "ccccccccccc" });
+        }
+    }
+}
---
+++ WpfListBoxSample/NotifyPropertyChanged.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp4
+{
+    public class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
